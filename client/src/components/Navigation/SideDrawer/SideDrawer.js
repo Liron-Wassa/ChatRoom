e1311f9b0a2a97f3dc023f406acf143e4836a886
Liron-Wassa <khruiuuxv@gmail.com>
2020-05-30 21:17:14 +0300

    added automate close bar navigation

--- client/src/components/Navigation/SideDrawer/SideDrawer.js
+++ client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,13 +1,15 @@
 import { ChatContext } from '../../../contexts/ChatContext';
 import BackDrop from '../../UI/BackDrop/BackDrop';
 import classes from './SideDrawer.module.css';
+import { withRouter } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import React, { useContext } from 'react';
+import queryString from 'query-string';
 import Logo from '../../UI/Logo/Logo';
 
-const SideDrawer = () => {
+const SideDrawer = (props) => {
     
-    const { show, userName } = useContext(ChatContext);
+    const { show, userName, setShow } = useContext(ChatContext);
 
     let attachedClasses = [classes.SideDrawer, classes.Close];
 
@@ -15,6 +17,8 @@ const SideDrawer = () => {
         attachedClasses = [classes.SideDrawer];
     }    
     
+    const parsed = queryString.parse(props.location.search);
+
     return (
         <React.Fragment>
             <BackDrop />
@@ -23,18 +27,49 @@ const SideDrawer = () => {
                     <Logo />
                 </div>
                 <ul>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=music`}><i className="fas fa-users"></i>Music</NavLink></li>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=sport`}><i className="fas fa-users"></i>Sport</NavLink></li>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=famous`}><i className="fas fa-users"></i>Famous</NavLink></li>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=food`}><i className="fas fa-users"></i>Food</NavLink></li>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=women`}><i className="fas fa-users"></i>Women</NavLink></li>
-                    <li><NavLink to={`/chatroom/?username=${userName}&room=men`}><i className="fas fa-users"></i>men</NavLink></li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=music`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'music' ? classes.active : null}
+                        ><i className="fas fa-users"></i>Music</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=sport`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'sport' ? classes.active : null}
+                        ><i className="fas fa-users"></i>Sport</NavLink></li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=famous`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'famous' ? classes.active : null}
+                        ><i className="fas fa-users"></i>Famous</NavLink></li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=food`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'food' ? classes.active : null}
+                        ><i className="fas fa-users"></i>Food</NavLink></li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=women`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'women' ? classes.active : null}
+                        ><i className="fas fa-users"></i>Women</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to={`/chatroom/?username=${userName}&room=men`}
+                            onClick={() => setShow(false)}
+                            activeClassName={parsed.room === 'men' ? classes.active : null}
+                        ><i className="fas fa-users"></i>men</NavLink>
+                    </li>
                 </ul>
             </nav>
         </React.Fragment>
     );
 };
 
-export default SideDrawer;
-
-// activeClassName={classes.active}
+export default withRouter(SideDrawer);
